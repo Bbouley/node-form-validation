1c4ec40d9a894469c46680addf4a26b26055ac71
bbouley <b.bouley@hotmail.co.uk>
2015-08-24 10:18:40 -0600

    added form validation on server side

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,6 @@ var puppyArray = [];
 
 var peopleArray = [];
 
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Add a Puppy' });
-// });
 
 router.get('/puppy/new', function(req, res, next){
   res.render('addpuppy');
@@ -17,8 +14,19 @@ router.post('/puppy/new', function(req, res, next){
   var name = req.body.puppyName;
   var id = req.body.puppyId;
   var puppy = new Puppy(name, id);
-  puppyArray.push(puppy);
-  res.redirect('/puppies');
+  var error1 = 'You didn\'t enter a name!';
+  var error2 = 'You didn\'t enter an ID!';
+  var test = emptyTestPuppy(name,id);
+    if(test === 'name blank'){
+      res.render('addpuppy', {error1:error1});
+    } else if(test === 'id blank'){
+      res.render('addpuppy', {error2:error2});
+    } else if (test === 'both blank'){
+      res.render('addpuppy', {error2:error2, error1:error1});
+    } else if(test === true){
+      puppyArray.push(puppy);
+      res.redirect('/puppies');
+    }
 });
 
 router.get('/puppies', function(req, res, next){
@@ -35,9 +43,19 @@ router.post('/people/new', function(req, res, next){
   var name = req.body.personName;
   var hobby = req.body.personHobby;
   var person = new Person(name, hobby);
-  peopleArray.push(person);
-  console.log(peopleArray);
-  res.redirect('/people');
+  var error1 = 'You didn\'t enter a name!';
+  var error2 = 'You didn\'t enter an hobby!';
+  var test = emptyTestPerson(name, hobby);
+   if(test === 'name blank'){
+      res.render('addperson', {error1:error1});
+    } else if(test === 'hobby blank'){
+      res.render('addperson', {error2:error2});
+    } else if (test === 'both blank'){
+      res.render('addperson', {error2:error2, error1:error1});
+    } else if(test === true){
+      peopleArray.push(person);
+      res.redirect('/people');
+    }
 });
 
 router.get('/people', function(req, res, next){
@@ -45,8 +63,6 @@ router.get('/people', function(req, res, next){
 });
 
 
-
-module.exports = router;
 
 var Puppy = function(name, id){
   this.name = name;
@@ -57,3 +73,30 @@ var Person = function(name, hobby){
   this.name = name;
   this.hobby = hobby;
 };
+
+function emptyTestPuppy(name, id){
+  if(id === '' && name === ''){
+      return 'both blank';
+  } else if (id === ''){
+      return 'id blank';
+  } else if(name === ''){
+      return 'name blank';
+  } else {
+    return true;
+  }
+}
+
+function emptyTestPerson(name, hobby){
+  if (name === '' && hobby === ''){
+    return 'both blank';
+  } else if (name === ''){
+    return 'name blank';
+  } else if (hobby === ''){
+    return 'hobby blank';
+  } else {
+    return true;
+  }
+}
+
+
+module.exports = router;
