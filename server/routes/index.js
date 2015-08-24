8eb666792c5ed27a02711934bd89f64f5e83d0a0
bbouley <b.bouley@hotmail.co.uk>
2015-08-23 20:07:19 -0600

    added person creator and table

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var puppyArray = [];
 
+var peopleArray = [];
+
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Add a Puppy' });
 // });
@@ -16,7 +18,6 @@ router.post('/puppy/new', function(req, res, next){
   var id = req.body.puppyId;
   var puppy = new Puppy(name, id);
   puppyArray.push(puppy);
-  console.log(puppyArray);
   res.redirect('/puppies');
 });
 
@@ -24,9 +25,24 @@ router.get('/puppies', function(req, res, next){
   res.render('showPuppies', {puppyArray : puppyArray});
 });
 
-router.get('/people/new', function(req, res, next){
 
-})
+router.get('/people/new', function(req, res, next){
+  res.render('addperson');
+});
+
+
+router.post('/people/new', function(req, res, next){
+  var name = req.body.personName;
+  var hobby = req.body.personHobby;
+  var person = new Person(name, hobby);
+  peopleArray.push(person);
+  console.log(peopleArray);
+  res.redirect('/people');
+});
+
+router.get('/people', function(req, res, next){
+  res.render('showPeople', {peopleArray : peopleArray});
+});
 
 
 
@@ -35,4 +51,9 @@ module.exports = router;
 var Puppy = function(name, id){
   this.name = name;
   this.id = id;
+};
+
+var Person = function(name, hobby){
+  this.name = name;
+  this.hobby = hobby;
 };
